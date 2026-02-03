7cb987ba1c64224388531c296399dd375373c7f5
Mb Next Time <sombrero.oval@gmail.com>
2026-02-03 15:55:06 +0700

    Added simple validation for user credentials

--- src/auth/schemas/user.py
+++ src/auth/schemas/user.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class UserBase(BaseModel):
-    username: str
+    username: str = Field(min_length=3, max_length=32, pattern=r'^[a-zA-Z0-9_\-.]+$')
 
 class UserIn(UserBase):
-    password: str = Field(min_length=8)
+    password: str = Field(min_length=8, max_length=64)
 
 class UserOut(UserBase):
     ...
